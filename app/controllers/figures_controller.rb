00c717bdd84d1c82d54eec780a9bc79d61eefae4
Brenda Ferrufino <ferrufinob@gmail.com>
2020-11-22 01:33:11 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,63 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  #Index
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  #New
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  #Create
+  post '/figures' do
+    figure = Figure.create(name: params[:figure][:name])
+
+    landmark_ids = params[:figure][:landmark_ids]
+    title_ids = params[:figure][:title_ids]
+   
+    if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+
+    if landmark_ids
+      landmark_ids.each do |l|
+        figure.landmarks << Landmark.find(l)
+      end
+    end
+
+    if !params[:title][:name].empty?
+      figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    if title_ids
+      title_ids.each do |t|
+        figure.titles << Title.find(t)
+      end
+      end
+
+    figure.save
+    redirect to "/figures/#{figure.id}"
+  end
+
+  #Show
+  get '/figures/:id' do
+    @figure = Figure_find_by_id(params[:id])
+    erb :'figures/show'
+  end
+
+  #Update/Edit
+  get '/figures/:id/edit' do
+    @figure = Figure_find_by_id(params[:id])
+    erb :'figures/edit'
+  end
+
+#   patch '/figures/:id'
+#   #update
+#   redirect to "/figures/#{figure.id}"
+# end
 end
